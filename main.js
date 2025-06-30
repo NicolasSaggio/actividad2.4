e0572083e013031f8a515340692d76462a0baf35
Nicolas Saggio <nicolas.p.saggio@gmail.com>
2025-06-30 14:19:31 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let boton = document.getElementById("boton");
+
+let mensajeInicial;
+
+boton.addEventListener("click", () => {
+  let numero = prompt("tu numero");
+  numero = Number(numero);
+  for (let i = numero; i > 0; i--) {
+    let respuesta = alert("hola");
+    respuesta;
+    console.log("hola");
+  }
+});
